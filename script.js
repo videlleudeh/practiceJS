c3242148d6e8ba8d838e0ca7fd700246af2dcbcf
videlleudeh <chizzyudeh03@gmail.com>
2022-06-21 19:58:35 +0100

    added prompt feature

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-document.write("Hi, My name is Chizoba Udechukwu. I am 5'9 and I am from Nigeria.")
+ document.write("Hi, My name is Chizoba Udechukwu. I am 5'9 and I am from Nigeria.")
+
+
+// alert("This is to return your name, height and country. Press 'okay' to continue.")
+
+// let userName = prompt("What is your FullName? ")
+// let userHeight = parseFloat(prompt("How tall are you(cm)?"))
+// let userCountry = prompt("What country are you from?")
+
+// document.write("Hi, My name is " + userName + "." + " I am " + userHeight + " tall and I am from " + userCountry + ".")
+
